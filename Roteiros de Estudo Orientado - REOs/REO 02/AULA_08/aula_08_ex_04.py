7a01b9b602f25484b4d1ae75641e009afaec2ae6
Vinícius Quintão Carneiro <vqcarneiro@gmail.com>
2020-08-09 12:08:51 -0300

    REO 03

--- Roteiros de Estudo Orientado - REOs/REO 02/AULA_08/aula_08_ex_04.py
+++ Roteiros de Estudo Orientado - REOs/REO 02/AULA_08/aula_08_ex_04.py
@@ -14,7 +14,7 @@
 # http://www.cmm.mines-paristech.fr/~beucher/wtshed.html
 ########################################################################################################################
 from skimage.feature import peak_local_max
-from skimage.morphology import watershed
+from skimage.segmentation import watershed
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
@@ -30,27 +30,40 @@ limiar, mascara = cv2.threshold(cr,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 
 ########################################################################################################################
 img_dist = ndimage.distance_transform_edt(mascara) # https://docs.scipy.org/doc/scipy/reference/generated/scipy.ndimage.distance_transform_edt.html
-# ndimage.distance_transform_edt: Como o nome sugere, essa função calcula a distância euclidiana até o zero mais próximo (ou seja, pixel de fundo)
-# para cada um dos pixels de primeiro plano.
+# ndimage.distance_transform_edt: Calcula a distância euclidiana até o zero
+# mais próximo (ou seja, pixel de fundo)para cada um dos pixels de primeiro plano.
 
 max_local = peak_local_max(img_dist, indices=False, min_distance=300,
 	labels=mascara)
-# https://scikit-image.org/docs/stable/api/skimage.feature.html#skimage.feature.peak_local_max
+# peak_local_max: retorna uma matriz booleana com os picos da imagem
+# baseados nas distâncias
 # min_distance: Número mínimo de pixels que separam os picos
+# https://scikit-image.org/docs/stable/api/skimage.feature.html#skimage.feature.peak_local_max
+print('-'*50)
+print('Número de Picos')
+print(np.unique(max_local,return_counts=True))
+print('-'*50)
 
 marcadores,n_marcadores = ndimage.label(max_local, structure=np.ones((3, 3)))
+# Realiza marcação dos picos
 # https://docs.scipy.org/doc/scipy/reference/generated/scipy.ndimage.label.html
 # https://en.wikipedia.org/wiki/Connected-component_labeling
+print('Análise de conectividade - Marcadores')
+print(np.unique(marcadores,return_counts=True))
+print('-'*50)
 
 img_ws = watershed(-img_dist, marcadores, mask=mascara)
-# https://scikit-image.org/docs/dev/api/skimage.morphology.html#skimage.morphology.watershed
+# https://scikit-image.org/docs/dev/api/skimage.segmentation.html#skimage.segmentation.watershed
 # https://en.wikipedia.org/wiki/Watershed_(image_processing)
 # https://scikit-image.org/docs/dev/auto_examples/segmentation/plot_watershed.html
 
+print('Imagem Segmentada - Watershed')
+print(np.unique(img_ws,return_counts=True))
 print("Número de Batatas: ", len(np.unique(img_ws)) - 1)
+print('-'*50)
 
 img_final = np.copy(img_rgb)
-img_final[img_ws != 3] = [0,0,0]
+img_final[img_ws != 3] = [0,0,0] # Acessando a batata 3
 ########################################################################################################################
 plt.figure('Watershed')
 plt.subplot(2,3,1)
